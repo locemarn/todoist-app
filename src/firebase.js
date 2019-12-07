5286f10b6c83fb09c1bacead70d5cdeb4666061e
locemarn <marcelon27@gmail.com>
2019-12-07 20:28:33 -0200

    firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import firebase from 'firebase/app'
 import 'firebase/firestore'
 
 const firebaseConfig = firebase.initializeApp({
-  apiKey: '',
-  authDomain: '',
-  databaseUrl: '',
-  projectId: '',
-  storageBucket: '',
-  messageSenderId: '',
-  apiId: '',
+  apiKey: "AIzaSyB2QCASotKaVsJ5utcqRDur6aduqZzdsnQ",
+  authDomain: "todoist-3e754.firebaseapp.com",
+  databaseURL: "https://todoist-3e754.firebaseio.com",
+  projectId: "todoist-3e754",
+  storageBucket: "todoist-3e754.appspot.com",
+  messagingSenderId: "476980176367",
+  appId: "1:476980176367:web:068ac3f0808f1d08d7821a"
 })
 
 export { firebaseConfig as firebase }
