2ee0c3cc01de189811e3f417d7b332b8b543d3f6
locemarn <marcelon27@gmail.com>
2019-12-14 10:31:16 -0200

    layout content

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,30 +1,34 @@
-import React, {useEffect} from 'react'
-import { Checkbox } from '../components/Checkbox'
-import { useTasks } from '../hooks'
-import { collatedTasks } from "../constants";
-import { getTitle, getCollatedTitle, collatedTasksExist } from "../helpers";
-import { useSelectedProjectValue, useProjectsValue } from "../context";
+import React, { useEffect } from 'react';
+import { Checkbox } from './Checkbox';
+// import { AddTask } from './AddTask';
+import { useTasks } from '../hooks';
+import { collatedTasks } from '../constants';
+import { getTitle, getCollatedTitle, collatedTasksExist } from '../helpers';
+import { useSelectedProjectValue, useProjectsValue } from '../context';
 
 export const Tasks = () => {
-  const {selectedProject} = useSelectedProjectValue()
-  const { projects } = useProjectsValue()
-  const { tasks } = useTasks(selectedProject)
+  const { selectedProject } = useSelectedProjectValue();
+  const { projects } = useProjectsValue();
+  const { tasks } = useTasks(selectedProject);
 
-  let projectName = ''
-
-  if (projects && selectedProject && !collatedTasksExist(selectedProject)) {
-    projectName = getTitle(projects, selectedProject).name
-    console.log('project name 1:', projectName)
-  }
+  let projectName = '';
 
   if (collatedTasksExist(selectedProject) && selectedProject) {
-    projectName = getCollatedTitle(collatedTasks, selectedProject).name
-    console.log('project name 2:', projectName)
+    projectName = getCollatedTitle(collatedTasks, selectedProject).name;
+  }
+
+  if (
+    projects &&
+    projects.length > 0 &&
+    selectedProject &&
+    !collatedTasksExist(selectedProject)
+  ) {
+    projectName = getTitle(projects, selectedProject).name;
   }
 
   useEffect(() => {
-    document.title = `${projectName}: Todoist`
-  }, [])
+    document.title = `${projectName}: Todoist`;
+  });
 
   return (
     <div className="tasks" data-testid="tasks">
@@ -38,6 +42,8 @@ export const Tasks = () => {
           </li>
         ))}
       </ul>
+
+      {/* <AddTask /> */}
     </div>
-  )
-}
+  );
+};
