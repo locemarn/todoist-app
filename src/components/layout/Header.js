67d192fdcfcbd025c8350500eb2223db6fa8ddf1
locemarn <marcelon27@gmail.com>
2019-12-14 17:24:14 -0200

    dark mode

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,24 +1,31 @@
-import React  from 'react';
+import React, { useState } from "react";
 import { FaPizzaSlice } from "react-icons/fa";
 
-export const Header = () => {
+export const Header = ({ darkMode, setDarkMode }) => {
+  const [shouldShowMain, setShouldShowMain] = useState(false);
+  const [showQuickAddTask, setShowQuickAddTask] = useState(false);
+
   return (
     <header className="header" data-testid="header">
       <nav>
         <div className="logo">
-          <img src="/images/logo.png" alt="Todoist"/>
+          <img src="/images/logo.png" alt="Todoist" />
         </div>
         <div className="settings">
           <ul>
             <li className="settings__add" data-testid="quick-add-task-action">
               +
             </li>
-            <li className="settings__darkmode" data-testid="dark-mode-action">
+            <li
+              onClick={() => setDarkMode(!darkMode)}
+              className="settings__darkmode"
+              data-testid="dark-mode-action"
+            >
               <FaPizzaSlice />
             </li>
           </ul>
         </div>
       </nav>
     </header>
-  )
-}
+  );
+};
