448517d841f51ab188d75cb595706f7b0a864b10
locemarn <marcelon27@gmail.com>
2019-12-16 23:43:28 -0200

    some more test to <AddTask /> component' '

--- src/components/TaskDate.js
+++ src/components/TaskDate.js
@@ -6,7 +6,7 @@ export const TaskDate = ({ setTaskDate, showTaskDate, setShowTaskDate }) =>
   showTaskDate && (
     <div className="task-date" data-testid="task-date-overlay">
       <ul className="task-date__list">
-        <li data-testid="task-date-overlay">
+        <li data-testid="task-date-today">
           <div
             onClick={() => {
               setShowTaskDate(false);
